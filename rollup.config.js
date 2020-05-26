0e2877bced19889906b460e5a40ca71b96b40e43
Dax Ravi <d@ironbay.co>
2020-05-25 21:11:47 -0400

    Initial commit

---
+++ rollup.config.js
@@ -0,0 +1,28 @@
+import typescript from "rollup-plugin-typescript2"
+import pkg from "./package.json"
+
+export default {
+  input: "src/index.ts",
+  output: [
+    {
+      banner: "#!/usr/bin/env node",
+      file: pkg.main,
+      format: "cjs",
+    },
+    // {
+    //     file: pkg.browser,
+    //     format: 'iife',
+    //     name: 'Riptide'
+    // }
+  ],
+  external: [
+    "fs",
+    ...Object.keys(pkg.dependencies),
+    ...Object.keys(pkg.peerDependencies),
+  ],
+  plugins: [
+    typescript({
+      typescript: require("typescript"),
+    }),
+  ],
+}
